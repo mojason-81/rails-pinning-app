4fdf2235d915800589af3a3409701c1469f7a78a
mojason <jason@mojason.com>
2015-08-12 21:51:19 -0500

    added edit route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   post '/login' => "users#authenticate"
 
+  get 'pins/id/edit' => 'pins#edit'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
