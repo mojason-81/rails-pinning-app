b15e6f1df7b3e3f4ad88d8865639f5d6dc985fa9
mojason <jason@mojason.com>
2015-08-19 20:46:47 -0500

    adding ability to repin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,8 @@ class Pin < ActiveRecord::Base
   validates_uniqueness_of :slug
   belongs_to :category
   belongs_to :user
+  has_many :pinnings
+  has_many :users, through: :pinnings
   has_attached_file :image, styles: { medium: "300x300>", thumb: "90x90>" }, default_url: "http://placebear.com/90/90"
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
